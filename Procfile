e3680e01e07c3d9c25281eca91058c7691976d81
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-07-10 14:57:48 -0400

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn nftBack_end.wsgi
--- nftBack_end/settings.py
+++ nftBack_end/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+import django_heroku
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,17 +83,24 @@ WSGI_APPLICATION = 'nftBack_end.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nftback_end',
-        'USER': 'aleksandarbulajic',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+DATABASE_URL=env('DATABASE_URL')
 
-    }
+DATABASES = {
+    'default':
+        dj_database_url.config('DATABASE_URL')
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'nftback_end',
+#         'USER': 'aleksandarbulajic',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '',
+
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
